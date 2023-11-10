2dbbf91273fb4091ad36a99930fe4ba8be97e1ef
jaye <jian.huang@sihua.tech>
2023-11-10 10:31:23 +0800

    微调

--- lib/wd_screen_button.dart
+++ lib/wd_screen_button.dart
@@ -7,27 +7,30 @@ typedef WDScreenButtonCallBack = void Function();
 class WDScreenButton {
 
   static OverlayEntry? _overlayEntry;
-  static bool _isShow = false;
+  ///是否显示
+  static bool isShow = false;
   static WDScreenButtonCallBack? _callBack;
   static GlobalKey<NavigatorState>? _globalKey;
 
   static double _dx = 30;
   static double _dy = 100;
   static Widget? _buttonChild;
+  ///是否初始化
   static bool isInit = false;
 
   ///显示/隐藏按钮
   static changeStatus(bool status){
-    _isShow = !status;
+    isShow = status;
     Future.delayed(Duration(milliseconds: 330)).then((value) {
       _overlayEntry?.markNeedsBuild();
     });
   }
   static initConfig(GlobalKey<NavigatorState> globalKey,{double left = 30, double top = 100,
-    Widget? buttonChild,WDScreenButtonCallBack? callBack}){
+    bool isShowBtn = false,Widget? buttonChild,WDScreenButtonCallBack? callBack}){
     if(isInit){
       return;
     }
+    isShow = isShowBtn;
     _callBack = callBack;
     _dx = left;
     _dy = top;
@@ -53,7 +56,7 @@ class WDScreenButton {
                 }
               },
               child: Offstage(
-                offstage: _isShow,
+                offstage: !isShow,
                 child: Draggable(
                     onDragUpdate: (DragUpdateDetails details){
                     },
